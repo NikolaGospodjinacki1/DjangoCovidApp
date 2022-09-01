ab56eed8b58b9e7e6965dc88fe96e22d2e8b13a2
NikolaGospodjinacki1 <nikola.gospodjinacki@levi9.com>
2022-09-01 15:15:40 +0200

    fuzzy search

--- covidapp/myapp/views.py
+++ covidapp/myapp/views.py
@@ -47,7 +47,7 @@ def export_data_to_csv(request, dataframe=pandas_filter_data()):
     query_dict = request.GET
     query = query_dict.get("query")
     if query:
-        dataframe = dataframe.query('Country.str.contains(@query)')
+        dataframe = dataframe.query('Country.str.contains(@query)', engine={'python'})
     response = HttpResponse(
                             content_type='text/csv',
                             headers={'Content-Disposition': f'attachment; filename="covid_data_{query}.csv"'}
